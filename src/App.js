0d6c2de3834d9a01dce6352d3ccbd3921295df92
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2025-01-12 14:20:05 -0500

    added some global styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Popular from './Components/Popular';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
     <div className="App">
       Hello World
+      <GlobalStyle/>
+      <Popular/>
     </div>
   );
 }
